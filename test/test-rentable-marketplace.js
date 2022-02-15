d08a9ee27f9f1bedd96973b58bce234d778bd5ba
Shiva <shanshiva@outlook.com>
2022-02-15 16:50:09 +0530

    Added Test to deploy RentalMarket Contract

--- test/test-rentable-marketplace.js
+++ test/test-rentable-marketplace.js
@@ -8,6 +8,11 @@ const { network  } = require("hardhat");
 describe("NFTMarket", function() {
   it("Should create and execute market sales", async function() {
 
+    const Market = await ethers.getContractFactory("RentalMarket")
+    const market = await Market.deploy()
+    await market.deployed()
+    const marketAddress = market.address
+
     const NFT = await ethers.getContractFactory("NFT")
     const nft = await NFT.deploy(marketAddress)
     await nft.deployed()
